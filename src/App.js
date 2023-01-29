d2216b27829bbf76a17ef9b976b25794d0a8fdb8
Shyla <shailajasrinivas91@gmail.com>
2023-01-29 17:05:17 -0500

    dependencies installed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddUser from "./pages/AddUser";
+import EditUser from "./pages/EditUser";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/addUser" component={AddUser} />
+				<Route exact path="/editUser/:id" component={EditUser} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
